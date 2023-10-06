61019e1ae14e0851ed4a90d4bee5bbaf83a25245
marekAren <marekf555@interia.pl>
2023-10-06 14:33:16 +0200

    31-js-zdarzenie-onclick last

---
+++ app.js
@@ -0,0 +1,22 @@
+
+// let header = document.querySelector('h1');
+// header.onclick() = function() {console.log('kliknieto')} 
+
+// document.querySelector('h1').onclick = function() {
+//     alert('Właśnie kliknąłeś tekst nagłówka h1');
+// }
+
+let button = document.querySelector('button')
+button.onclick = function() {
+    alert ('klik działa!')
+} 
+
+
+// let przycisk = document.querySelector('button');
+// // przycisk.onclick = () => {alert('klikniety strzałkowa')};
+// przycisk.onclick = () => {console.log('klikniety strzałkowa')};
+
+
+// let przycisk1 = document.getElementById('button1');
+// // przycisk1.onclick = () => {alert('klikniety strzałkowa przycisk1')};
+// przycisk1.onclick = () => {console.log('klikniety strzałkowa przycisk1')};
